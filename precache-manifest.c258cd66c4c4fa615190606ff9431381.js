80445ba3a5221331714ca8ff23562d69306900b9
Val <50718596+ValerDev@users.noreply.github.com>
2019-07-09 22:45:22 +0300

    Add files via upload

---
+++ precache-manifest.c258cd66c4c4fa615190606ff9431381.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49765385dabf14a171eedabdc112a17f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b39229acd309d116ac95",
+    "url": "/static/css/main.6b99b6fc.chunk.css"
+  },
+  {
+    "revision": "5a59ff517d3b0db0a0f9",
+    "url": "/static/js/2.04204285.chunk.js"
+  },
+  {
+    "revision": "b39229acd309d116ac95",
+    "url": "/static/js/main.7ebb81da.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
